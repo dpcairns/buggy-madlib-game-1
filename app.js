ea664b1fe9f641f3b4bb6fafc4f8e8ef578a14ac
Jenna Goldman <jenna.goldman89@gmail.com>
2020-01-10 11:03:46 -0800

    add scripts to populate input values in story

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-// getElementById all of your spans and inputs
+const spanvar = document.getElementById('span');
+const inputvar = document.getElementById('adj1');
+
+const spanadjvar = document.getElementById('noun-1-span');
+const inputadjvar = document.getElementById('noun-1-input');
+
+const buttonvar = document.getElementById('button');
+
+const changefunc = () => {
+    spanvar.textContent = inputvar.value;
+    spanadjvar.textContent = inputadjvar.value;
+}
+
+buttonvar.addEventListener('click', changefunc)
+
+
+
+//const adj1Output = adj1Input.value;
 
 
 // Add an event listener to your button
+
+
+
+
+
+
+
+
+
+// add an event listener to the button
+
 
 // In the event listen, put all the input.values into the span.textContent elements
